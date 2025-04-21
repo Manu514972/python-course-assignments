9b7c0a04b2f29f926b6257a1979608a13cab83b9
Manu514972 <emmanuelamzallag61@gmail.com>
2025-04-21 22:50:15 +0300

    Create rectangle.py

---
+++ day03/rectangle.py
@@ -0,0 +1,7 @@
+import sys
+
+height = float(sys.argv[1])
+width = float(sys.argv[2])
+area = height*width
+perimeter = 2*width+2*height
+print("The area of the rectangle =", area, "and its perimeter =", perimeter)
