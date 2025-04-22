d09592a8bac6b0b5a2ea2643f40801be396a53d4
Manu514972 <emmanuelamzallag61@gmail.com>
2025-04-22 16:01:46 +0300

    Create number_guessing_game_0.py

---
+++ day03/number_guessing_game_0.py
@@ -0,0 +1,10 @@
+import random
+
+secret_number = random.randint(1, 20)
+guess = int(input("Guess a number between 1 and 20: "))
+if guess < secret_number:
+    print("Too low")
+elif guess > secret_number:
+    print("Too high")
+else:
+    print("Correct")
